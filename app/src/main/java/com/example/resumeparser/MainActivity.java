21184ce9f752b5189296479f245da984df20bcf2
Parth2712 <parth.bhambure@somaiya.edu>
2021-08-25 16:32:27 +0530

    upload file done

--- app/src/main/java/com/example/resumeparser/MainActivity.java
+++ app/src/main/java/com/example/resumeparser/MainActivity.java
@@ -10,10 +10,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -23,13 +31,31 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String urlFetch = "https://resume-parserapp.herokuapp.com/api/resume-list/";
+    private static final String urlUpload = "https://resume-parserapp.herokuapp.com/api/resume-create/";
 
     RecyclerView recyclerView;
     FloatingActionButton fabUpload;
     SwipeRefreshLayout swipeRefresh;
+
+    private ArrayList<HashMap<String, String>> arraylist;
+    private RequestQueue rQueue;
+    String url = "https://www.google.com";
 
     //This is for asking Storage Permission
     private static final int MY_RESULT_CODE_FILECHOOSER = 2000;
@@ -58,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                UploadFile();
+                UploadFileIntent();
 
             }
         });
@@ -99,13 +125,150 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onActivityResult(ActivityResult result) {
                     Toast.makeText(MainActivity.this, result.toString(), Toast.LENGTH_LONG).show();
+
+                    if (result.getResultCode() == RESULT_OK) {
+                        // Get the Uri of the selected file
+                        Uri uri = result.getData().getData();
+                        String uriString = uri.toString();
+                        File myFile = new File(uriString);
+                        String path = myFile.getAbsolutePath();
+                        String displayName = null;
+
+                        if (uriString.startsWith("content://")) {
+                            Cursor cursor = null;
+                            try {
+                                cursor = getContentResolver().query(uri, null, null, null, null);
+                                if (cursor != null && cursor.moveToFirst()) {
+                                    displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                                    Log.d("nameeeee>>>>  ",displayName);
+
+                                    UploadFile(displayName,uri);
+                                }
+                            } finally {
+                                cursor.close();
+                            }
+                        } else if (uriString.startsWith("file://")) {
+                            displayName = myFile.getName();
+                            Log.d("nameeeee>>>>  ",displayName);
+                        }
+                    }
+
+                    //super.onActivityResult(result);
+
+
                 }
             }
     );
 
+    private void UploadFile(String pdfname, Uri pdffile) {
+
+
+        InputStream iStream = null;
+        try {
+
+            iStream = getContentResolver().openInputStream(pdffile);
+            final byte[] inputData = getBytes(iStream);
+
+            VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, urlUpload,
+                    new Response.Listener<NetworkResponse>() {
+                        @Override
+                        public void onResponse(NetworkResponse response) {
+                            Log.d("ressssssoo",new String(response.data));
+                            rQueue.getCache().clear();
+                            try {
+                                JSONObject jsonObject = new JSONObject(new String(response.data));
+                                Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+
+                                jsonObject.toString().replace("\\\\","");
+
+                                if (jsonObject.getString("status").equals("true")) {
+                                    Log.d("come::: >>>  ","yessssss");
+                                    arraylist = new ArrayList<HashMap<String, String>>();
+                                    JSONArray dataArray = jsonObject.getJSONArray("data");
+
+
+                                    for (int i = 0; i < dataArray.length(); i++) {
+                                        JSONObject dataobj = dataArray.getJSONObject(i);
+                                        url = dataobj.optString("pathToFile");
+                                        //tv.setText(url);
+                                    }
+
+
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }) {
+
+                /*
+                 * If you want to add more parameters with the image
+                 * you can do it here
+                 * here we have only one parameter with the image
+                 * which is tags
+                 * */
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+
+                    params.put("resume", "/media/files/" + pdfname);
+
+                    // params.put("tags", "ccccc");  add string parameters
+                    return params;
+                }
+
+                /*
+                 *pass files using below method
+                 * */
+                @Override
+                protected Map<String, DataPart> getByteData() {
+                    Map<String, DataPart> params = new HashMap<>();
+
+                    params.put("resume", new DataPart(pdfname ,inputData));
+                    return params;
+                }
+            };
+
+
+            volleyMultipartRequest.setRetryPolicy(new DefaultRetryPolicy(
+                    0,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            rQueue = Volley.newRequestQueue(MainActivity.this);
+            rQueue.add(volleyMultipartRequest);
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public byte[] getBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+
+        int len = 0;
+        while ((len = inputStream.read(buffer)) != -1) {
+            byteBuffer.write(buffer, 0, len);
+        }
+        return byteBuffer.toByteArray();
+    }
+
 
     //File Picker       Work In Progress.........
-    private void UploadFile() {
+    private void UploadFileIntent() {
 
 
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
